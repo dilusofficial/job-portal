9ed22af77772269f39e266c1fef9d98769425fbc
clawlynx <clawlynx77@gmail.com>
2024-06-13 20:18:46 +0530

    final colour changes 2

--- client/src/components/DetailElement.jsx
+++ client/src/components/DetailElement.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function DetailElement({ title, content }) {
   return (
     <div className="flex gap-3 justify-start items-center my-3">
-      <p>{title}</p>
+      <p className="min-w-20">{title}</p>
       <p>:</p>
       <p>{content}</p>
     </div>
